13b07f1dd2936e8f993477a3b096a5f25ae6bcad
luiz <afsilvaluiz@gmail.com>
2024-02-14 11:05:20 -0300

    feat: query params

--- src/server.js
+++ src/server.js
@@ -11,5 +11,11 @@ app.get('/message/:id/:user', (request, response) => {
   `);
 });
 
+app.get('/users', (request, response) => {
+  const { page, limit } = request.query;
+
+  response.send(`Page: ${page}. Show: ${limit}`);
+});
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
