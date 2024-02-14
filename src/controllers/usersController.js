1a515d6d35ceb9d0e98f9da80416dd7cae63ab83
luiz <afsilvaluiz@gmail.com>
2024-02-14 14:38:16 -0300

    refactor: name file

--- src/controllers/usersController.js
+++
@@ -1,9 +0,0 @@
-class UsersController {
-  create(request, response) {
-    const { name, email, password } = request.body;
-
-    response.json({ name, email, password });
-  }
-}
-
-module.exports = UsersController;
