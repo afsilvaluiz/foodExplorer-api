24d3d7a1319ee168c7dcc2301d0176872380278b
luiz <afsilvaluiz@gmail.com>
2024-02-19 16:45:50 -0300

    feat: update user

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,27 +1,62 @@
 const { hash } = require('bcryptjs');
 
 const AppError = require('../utils/AppError');
-const sqliteConnection = require('../database/sqlite')
+const sqliteConnection = require('../database/sqlite');
 
 class UsersController {
   async create(request, response) {
-    const { name, email, password, is_admin = false } = request.body;
+    const {
+      name, email, password, is_admin = false,
+    } = request.body;
 
     const database = await sqliteConnection();
-    const checkUsersExists = await database.get('SELECT * FROM users WHERE email = (?)', [email])
+    const checkUsersExists = await database.get('SELECT * FROM users WHERE email = (?)', [email]);
 
-    if(checkUsersExists) {
-      throw new AppError('This email is already in use')
+    if (checkUsersExists) {
+      throw new AppError('This email is already in use');
     }
 
     const hashedPassword = await hash(password, 8);
 
     await database.run(
       'INSERT INTO users (name, email, password, is_admin) VALUES (?, ?, ?, ?)',
-      [name, email, hashedPassword, is_admin]
+      [name, email, hashedPassword, is_admin],
     );
 
     return response.status(202).json();
+  }
+
+  async update(request, response) {
+    const { name, email } = request.body;
+    const { id } = request.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id]);
+
+    if (!user) {
+      throw new AppError('User not found');
+    }
+
+    const userWithUpdatedEmail = await database.get('SELECT * FROM users WHERE email = (?)', [email]);
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError('This email is already in use');
+    }
+
+    user.name = name;
+    user.email = email;
+
+    await database.run(
+      `
+        UPDATE users SET
+        name = ?,
+        email = ?,
+        updated_at = ?
+        WHERE id = ?`,
+      [user.name, user.email, new Date(), id],
+    );
+
+    return response.json();
   }
 }
 
